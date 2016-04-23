acf6f6b3f80c781149d25e2694be25ad3bb596d6
murfman <murfman@gmail.com>
2016-04-22 23:36:54 -0500

    change to tput

--- pass_gen.sh
+++ pass_gen.sh
@@ -2,24 +2,73 @@
 
 # Random Password gnertor script with options...
 
+#new tput test code
+
+# clear screen
+tput clear
+
+# move cursor
+tput cup 3 15
+
+# set color
+tput setaf 3
+echo "*** Murph's ***"
+tput sgr0
+
+tput cup 5 17
+tput rev
 echo "*** Random Password Generator ***"
+tput sgr0
 
-echo "Choose an option below."
-echo $'\n'
-#questions to set Variables
+tput cup 7 15
 echo "What method do you want to use?"
-echo -e "\n"
-echo "1. Hash and base64 the date."
-echo "2. Use urandom."
-echo "3. Use openssl's rand function."
-echo "4. A reverse urandom"
-echo "5. Filter a string from urandom."
-echo "6. Use dd then base64."
-echo "7. MD5 the current date."
-echo " "
-read -p "Enter option between 1 and 7. " m1
 
+tput cup 8 15
+echo "1. Hash and base64 the date."
+tput cup 9 15
+echo "2. Use urandom."
+tput cup 10 15
+echo "3. Use openssl's rand function."
+tput cup 11 15
+echo "4. A reverse urandom"
+tput cup 12 15
+echo "5. Filter a string from urandom."
+tput cup 13 15
+echo "6. Use dd then base64."
+tput cup 14 15
+echo "7. MD5 the current date."
+
+tput bold
+tput cup 16 15
+read -p "Enter option between 1 and 7. " m1
+tput cup 17 15
 read -p "How many characters in the password?  " n1
+
+tput clear
+tput sgr0
+tput rc
+echo "Your password is: "
+
+# old echo code
+#
+# echo "*** Random Password Generator ***"
+#
+# echo "Choose an option below."
+# echo $'\n'
+# #questions to set Variables
+# echo "What method do you want to use?"
+# echo -e "\n"
+# echo "1. Hash and base64 the date."
+# echo "2. Use urandom."
+# echo "3. Use openssl's rand function."
+# echo "4. A reverse urandom"
+# echo "5. Filter a string from urandom."
+# echo "6. Use dd then base64."
+# echo "7. MD5 the current date."
+# echo " "
+# read -p "Enter option between 1 and 7. " m1
+#
+# read -p "How many characters in the password?  " n1
 
 if [[ $m1 -lt 1 ]]
 then
